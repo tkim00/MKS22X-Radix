75a29b652712d8a69516e8a3041459e8f07d6d8b
Taejoon Kim <tkim00@stuy.edu>
2019-04-02 16:48:57 -0400

    finding the maxDigit so far

--- Radix.java
+++ Radix.java
@@ -1,5 +1,20 @@
+import java.lang.*;
+
 public class Radix {
+  @SuppressWarnings("unchecked")
   public static void radixsort(int[]data){
-    
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
+    int max = 0;
+    for (int i = 0; i < data.length; i++) {
+      if (Math.abs(data[i]) > max) max = Math.abs(data[i]);
+    }
+    int maxDigit = 0;
+    while (max != 0) {
+      max = max/10;
+      maxDigit++;
+    }
+    for (int i = 0; i < maxDigit; i++) {
+
+    }
   }
 }
